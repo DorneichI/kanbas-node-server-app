ae6df535da819077d48fcff5a9fe5d7becb1d0ed
Immanuel Dorneich <immanuel.dorneich@gmail.com>
2024-03-28 19:43:19 -0400

    updated import in App.js

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import express from 'express';
 import cors from "cors";
 import Hello from "./Hello.js"
 import Lab5 from './Lab5.js';
-import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
+import CourseRoutes from './Kanbas/courses/routes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
