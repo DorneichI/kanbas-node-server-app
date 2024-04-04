466592442b7e5915764f860d6570345d038c1269
Immanuel Dorneich <immanuel.dorneich@gmail.com>
2024-04-04 17:13:06 -0400

    a6 Mongo su23

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import express from 'express';
+import mongoose from "mongoose";
+import session from "express-session";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
 import cors from "cors";
 import Hello from "./Hello.js"
 import Lab5 from './Lab5.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import CourseRoutes from './Kanbas/courses/routes.js';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+  
+const mongoURL1 = "mongodb+srv://<username>:<password>@kanbas.uvkddez.mongodb.net/?retryWrites=true&w=majority&appName=kanbas";
+const mongoURL2 = "mongodb+srv://<username>:<password>@kanbas.uvkddez.mongodb.net/"
+  
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app)
 Hello(app); 
